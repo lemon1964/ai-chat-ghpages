fabf3fd5a281f52c49681b2ffa83b9b59ec0e45b
lemon1964 <lemon1964@users.noreply.github.com>
2025-06-25 23:39:50 +0000

    deploy: 6d23c9967b172b8ef559b687dddfbe9d8bb90991

--- src/Components/providers/ClientReduxProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client"; // Делаем компонент клиентским
-
-import { Provider } from "react-redux";
-import store from "../../store/store";
-
-export default function ClientReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
--- src/utils/formatFileUrl.ts
+++
@@ -1,4 +0,0 @@
-// ai-chat-ghpages/src/utils/formatFileUrl.ts
-export const formatFileUrl = (fileUrl: string) => {
-  return fileUrl;
-};
