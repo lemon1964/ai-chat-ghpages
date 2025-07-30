e6f64e2072926ff7aced4c0ab85a48fca95c0b4d
lemon <lemon@example.com>
2025-07-30 14:03:27 +0300

    нейра

--- src/utils/formatFileUrl.ts
+++ src/utils/formatFileUrl.ts
@@ -1,3 +1,3 @@
 // ai-chat-ghpages/src/utils/formatFileUrl.ts
 export const formatFileUrl = (fileUrl: string): string => 
-  `/${process.env.NODE_ENV === 'production' ? 'ai-chat-pages/' : ''}${fileUrl.replace(/^\//, '')}`;
+  `/${process.env.NODE_ENV === 'production' ? 'ai-chat-ghpages/' : ''}${fileUrl.replace(/^\//, '')}`;
