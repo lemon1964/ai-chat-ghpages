8487706fbb9fd95362dfaf0745f96308d5cb086d
lemon <lemon@example.com>
2025-08-21 18:03:38 +0300

    final no codes

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// ai-chat-ghpages/src/app/page.tsx
-import ClientChatPage from "@features/common/ClientChatPage";
-
-export default async function Page() {
-  return <ClientChatPage />;
-}
--- src/utils/formatFileUrl.ts
+++
@@ -1,3 +0,0 @@
-// ai-chat-ghpages/src/utils/formatFileUrl.ts
-export const formatFileUrl = (fileUrl: string): string => 
-  `/${process.env.NODE_ENV === 'production' ? 'ai-chat-ghpages/' : ''}${fileUrl.replace(/^\//, '')}`;
